f9b9c49970ecf8a1f13f354e48a691e5cc1a2229
Jerrel <jerreljanssen2@gmail.com>
2024-04-21 23:09:02 +0200

    floor class created

---
+++ Blazor/Components/Room.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor.Components;
+
+public class Room
+{
+    public int FloorNumber { get; }
+    public string RoomType { get; }
+
+    public Room(int floorNumber, string roomType)
+    {
+        FloorNumber = floorNumber;
+        RoomType = roomType;
+    }
+}
