14dd730ed972d99f311da85e7b3c7a1213d44536
SubRider <tomzuidema2303@gmail.com>
2024-04-22 16:47:22 +0200

    Added images to overview, Added types to Room and ID, Added non functional code to WorkspaceOverview

--- Blazor/Components/Data/Room.cs
+++ Blazor/Components/Data/Room.cs
@@ -5,11 +5,13 @@ public class Room
     public int ID { get; }
     public int FloorNumber { get; }
     public int RoomNumber { get;  }
+    public string Type { get; }
     
-    public Room(int floorNumber, int roomNumber)
+    public Room(int floorNumber, int roomNumber, string type = "default")
     {
         FloorNumber = floorNumber;
         RoomNumber = roomNumber;
+        Type = type;
         ID = 1;
     }
 }
