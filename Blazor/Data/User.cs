1a9708b4b3aa36d92a9fecfcece60d721e16dc69
SubRider <1063617@hr.nl>
2024-05-21 09:04:56 +0200

    Hopefully working reservation system basics

--- Blazor/Data/User.cs
+++ Blazor/Data/User.cs
@@ -3,9 +3,10 @@ using Microsoft.AspNetCore.Identity;
 namespace Blazor.Data;
 
 // Add profile data for application users by adding properties to the User class
-public class User : IdentityUser
+public class User(string name) : IdentityUser
 {
     public bool IsAdmin = false;
+    public string Name = name;
 
     public Reservation Book(IBookable bookable, DateTime startDate, DateTime endDate)
     {
--- Blazor/Services/ReservationService.cs
+++ Blazor/Services/ReservationService.cs
@@ -10,4 +10,29 @@ public class ReservationService(IDbContextFactory<AppDbContext> contextFactory)
         await using var context = await contextFactory.CreateDbContextAsync();
         return await context.Reservations.ToListAsync();
     }
+    
+    public async Task<Reservation?> GetReservationByIdAsync(Guid id)
+    {
+        await using var context = await contextFactory.CreateDbContextAsync();
+        return await context.Reservations.FirstOrDefaultAsync(r => r.Id == id);
+    }
+    
+    public async Task<Reservation> CreateReservationAsync(Reservation reservation)
+    {
+        await using var context = await contextFactory.CreateDbContextAsync();
+        await context.Reservations.AddAsync(reservation);
+        await context.SaveChangesAsync();
+        return reservation;
+    }
+    
+    public async Task DeleteReservationAsync(Guid id)
+    {
+        await using var context = await contextFactory.CreateDbContextAsync();
+        var reservation = await context.Reservations.FirstOrDefaultAsync(r => r.Id == id);
+        if (reservation is not null)
+        {
+            context.Reservations.Remove(reservation);
+            await context.SaveChangesAsync();
+        }
+    }
 }
