b2b92563f2cb1bdc3aac271452048ac73982b98f
StinkyVoet <stinkyvoet@gmail.com>
2024-05-21 11:13:00 +0200

    Fixed reservations

--- Blazor/Data/User.cs
+++ Blazor/Data/User.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Identity;
 namespace Blazor.Data;
 
 // Add profile data for application users by adding properties to the User class
-public class User(string name) : IdentityUser
+public class User : IdentityUser
 {
     public bool IsAdmin = false;
-    public string Name = name;
-
+    
+    public List<Reservation> Reservations { get; set; }
     public Reservation Book(IBookable bookable, DateTime startDate, DateTime endDate)
     {
         return new Reservation();
