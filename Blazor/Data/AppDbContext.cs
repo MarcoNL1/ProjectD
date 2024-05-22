7448abdf12332989e8b0afdde8999e17879a0c9f
StinkyVoet <stinkyvoet@gmail.com>
2024-05-22 12:54:15 +0200

    Finished reservations

--- Blazor/Data/AppDbContext.cs
+++ Blazor/Data/AppDbContext.cs
@@ -8,4 +8,15 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbCo
     public DbSet<Workspace> Workspaces { get; init; } = default!;
     public DbSet<Room> Rooms { get; init; } = default!;
     public DbSet<Reservation> Reservations { get; init; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<User>()
+            .HasMany(u => u.Reservations)
+            .WithOne(r => r.User)
+            .HasForeignKey(r => r.UserId)
+            .IsRequired();
+    }
 }
