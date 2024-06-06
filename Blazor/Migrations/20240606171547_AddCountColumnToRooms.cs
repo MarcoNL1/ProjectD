f0f164d5441f85ba6cad4dfd83176f71257d7f25
Jerrel <jerreljanssen2@gmail.com>
2024-06-06 21:53:06 +0200

    added count at room db. added tables to see total reservations(room,wing,floor)

---
+++ Blazor/Migrations/20240606171547_AddCountColumnToRooms.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountColumnToRooms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Count",
+                table: "Rooms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "Rooms");
+        }
+    }
+}
